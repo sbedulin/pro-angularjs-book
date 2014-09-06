9be3aaa3d75890c9d81f10d999294a2a5d39ac09
Sergey Bedulin <serge.bedulin@gmail.com>
2014-09-06 11:56:48 +0300

    Adding new todo items

--- app/chapter-02/todo.js
+++ app/chapter-02/todo.js
@@ -22,6 +22,10 @@ todoApp.controller('ToDoCtrl', function ($scope) {
     $scope.warningLevel = function() {
         return $scope.incompleteCount() < 3 ? 'label-success' : 'label-warning';
     };
+
+    $scope.addNewItem = function(actionText) {
+        $scope.todo.items.push({ action: actionText, done: false });
+    };
 });
 
 todoApp.filter('boolToString', function() {
