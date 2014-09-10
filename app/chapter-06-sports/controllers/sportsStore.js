b8400250174dc94a61fcbc42a63d5c2f970222ee
Sergey Bedulin <serge.bedulin@gmail.com>
2014-09-10 14:11:26 +0300

    Get data from api

--- app/chapter-06-sports/controllers/sportsStore.js
+++ app/chapter-06-sports/controllers/sportsStore.js
@@ -1,25 +1,13 @@
 angular.module('sportsStore')
-.controller('sportsStoreCtrl', function($scope) {
-    $scope.data = {
-        products: [
-            { name: "Product #4", description: "A product",
-                category: "Category #3", price: 202 },
-            { name: "Product #1", description: "A product",
-                category: "Category #1", price: 100 },
-            { name: "Product #2", description: "A product",
-                category: "Category #1", price: 110 },
-            { name: "Product #4.1", description: "A product",
-                category: "Category #3", price: 202 },
-            { name: "Product #1.1", description: "A product",
-                category: "Category #1", price: 100 },
-            { name: "Product #2.1", description: "A product",
-                category: "Category #1", price: 110 },
-            { name: "Product #3", description: "A product",
-                category: "Category #2", price: 210 },
-            { name: "Product #4", description: "A product",
-                category: "Category #2", price: 211 },
-            { name: "Product #5", description: "A product",
-                category: "Category #2", price: 212 }
-        ]
-    };
-});
+    .constant('dataUrl', 'http://localhost:5000/products')
+    .controller('sportsStoreCtrl', function($scope, $http, dataUrl) {
+        $scope.data = {};
+
+        $http.get(dataUrl)
+            .success(function(data) {
+                $scope.data.products = data;
+            })
+            .error(function(error) {
+                $scope.data.error = error;
+            });
+    });
