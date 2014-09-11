bc8dba8dfceb21701d332c08649af7271ee6a805
Sergey Bedulin <serge.bedulin@gmail.com>
2014-09-11 19:49:34 +0300

    Added Cart component

---
+++ app/chapter-06-sports/components/cart/cart.js
@@ -0,0 +1,66 @@
+angular.module('cart', [])
+    .constant('componentPath', 'components/cart/')
+    .factory('cart', function() {
+        var cartData = [];
+
+        return {
+            addProduct: function(id, name, price) {
+                var addedToExistingItem = false;
+                for(var i = 0; i < cartData.length; i++) {
+                    if(cartData[i].id == id) {
+                        cartData[i].count++;
+                        addedToExistingItem = true;
+                        break;
+                    }
+                }
+                if(!addedToExistingItem) {
+                    cartData.push({
+                        count: 1,
+                        id: id,
+                        price: price,
+                        name: name
+                    });
+                }
+            },
+
+            removeProduct: function(id) {
+                for(var i = 0; i < cartData.length; i++) {
+                    if(cartData[i].id == id) {
+                        cartData.splice(i, 1);
+                        break;
+                    }
+                }
+            },
+
+            getProducts: function() {
+                return cartData;
+            }
+        };
+    })
+    .directive('cartSummary', function(cart, componentPath) {
+        return {
+            restrict: 'E',
+            templateUrl: componentPath + 'cartSummary.html',
+            controller: function($scope) {
+                var cartData = cart.getProducts();
+
+                $scope.stylePath = componentPath + 'cart.css';
+
+                $scope.total = function() {
+                    var total = 0;
+                    for(var i = 0; i < cartData.length; i++) {
+                        total += (cartData[i].price * cartData[i].count);
+                    }
+                    return total;
+                };
+
+                $scope.itemCount = function() {
+                    var total = 0;
+                    for(var i = 0; i < cartData; i++) {
+                        total += cartData[i].count;
+                    }
+                    return total;
+                };
+            }
+        };
+    });
