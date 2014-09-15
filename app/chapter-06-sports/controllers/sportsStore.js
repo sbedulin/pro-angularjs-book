0035019e6c0fb45c754d7325e3dc40201b6e8e7e
Sergey Bedulin <serge.bedulin@gmail.com>
2014-09-15 13:03:42 +0300

    Added ability to place orders

--- app/chapter-06-sports/controllers/sportsStore.js
+++ app/chapter-06-sports/controllers/sportsStore.js
@@ -1,6 +1,8 @@
 angular.module('sportsStore')
     .constant('dataUrl', 'http://localhost:5000/products')
-    .controller('sportsStoreCtrl', function($scope, $http, dataUrl) {
+    .constant('orderUrl', 'http://localhost:5000/orders')
+    .controller('sportsStoreCtrl', function($scope, $http, $location,
+        dataUrl, orderUrl, cart) {
         $scope.data = {};
 
         $http.get(dataUrl)
@@ -11,4 +13,20 @@ angular.module('sportsStore')
                 error = error || { status: 'Unknown error' }
                 $scope.data.error = error;
             });
+
+        $scope.sendOrder = function(shippingDetails) {
+            var order = angular.copy(shippingDetails);
+            order.products = cart.getProducts();
+            $http.post(orderUrl, order)
+                .success(function(data) {
+                    $scope.data.orderId = data.id;
+                    cart.getProducts().length = 0;
+                })
+                .error(function(error) {
+                    $scope.data.orderError = error;
+                })
+                .finally(function() {
+                    $location.path('/complete')
+                });
+        };
     });
