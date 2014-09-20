6df8c22a038f76ebffe825b035a6168ee919a151
Sergey Bedulin <serge.bedulin@gmail.com>
2014-09-20 20:20:17 +0300

    Added admin area

---
+++ app/chapter-06-sports/controllers/adminControllers.js
@@ -0,0 +1,16 @@
+angular.module('sportsStoreAdmin')
+    .constant('authUrl', 'http://localhost:5000/users/login')
+    .controller('authCtrl', function($scope, $http, $location, authUrl) {
+        $scope.authenticate = function(user, pass) {
+            $http.post(authUrl, {
+                username: user,
+                password: pass
+            }, {
+                withCredentials: true
+            }).success(function(data) {
+                $location.path('/main');
+            }).error(function(error) {
+                $scope.authenticationError = error;
+            });
+        };
+    });
