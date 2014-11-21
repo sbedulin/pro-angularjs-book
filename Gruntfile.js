675360c86f536d99d00ff7bc76840acfbd302f10
Sergey Bedulin <serge.bedulin@gmail.com>
2014-11-21 16:23:14 +0300

    Pre-commit git hook

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,14 +18,25 @@ module.exports = function(grunt) {
             gitCommit: {
                 command: "git commit README.md -m 'Updated readme'"
             }
+        },
+        jshint: {
+            files: [
+                'app/chapter-19/*.js' // Only apply tests to angular scripts for now
+            ],
+            options: {
+                jshintrc: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-git');
     grunt.loadNpmTasks('grunt-text-replace');
     grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     grunt.registerTask('default', function() {
         grunt.task.run('replace', 'shell:gitAdd', 'shell:gitCommit');
     });
+
+    grunt.registerTask('validatejs', ['jshint']);
 };
